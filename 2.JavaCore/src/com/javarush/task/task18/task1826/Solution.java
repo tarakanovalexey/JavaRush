68e337e3cf8848f5f93b54802e695f0a847feb2e
Aleksey Tarakanov <zcrewhtc@gmail.com>
2019-03-13 21:03:07 +0300

    Решаю из дома

--- 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1826/Solution.java
@@ -4,8 +4,21 @@ package com.javarush.task.task18.task1826;
 Шифровка
 */
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileInputStream input = new FileInputStream(args[1]);
+        FileOutputStream output = new FileOutputStream(args[2]);
+        if (args[0].equals("-e"))
+            while(input.available() > 0)
+                output.write(input.read()+1);
+            else while (input.available() > 0)
+                output.write(input.read()-1);
+            input.close();
+            output.close();
 
     }
 
