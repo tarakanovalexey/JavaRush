35a590f7f4104a588a4951b112ccf0a09ddc72b3
Aleksey Tarakanov <zcrewhtc@gmail.com>
2019-03-20 04:47:58 +0300

    Решаю из дома

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -15,22 +15,56 @@ public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader R = new BufferedReader(new InputStreamReader(System.in));
         String file1 = R.readLine();
         String file2 = R.readLine();
         R.close();
+
+        List<String> arr1 = new ArrayList<>();
         BufferedReader input1 = new BufferedReader(new FileReader(file1));
-        BufferedReader input2 = new BufferedReader(new FileReader(file2));
-
         while (input1.ready())
-
+            arr1.add(input1.readLine());
         input1.close();
-        while (input2.ready())
 
+        List<String> arr2 = new ArrayList<>();
+        BufferedReader input2 = new BufferedReader(new FileReader(file2));
+        while (input2.ready())
+            arr2.add(input2.readLine());
         input2.close();
 
-    }
+        while (arr1.size() > 0 && arr2.size() > 0) {
 
+            if (arr1.get(0).equals(arr2.get(0))) {
+                lines.add(new LineItem(Type.SAME, arr1.get(0)));
+                arr1.remove(0);
+                arr2.remove(0);
+            } else {
+                if (arr1.size() > 0) {
+                    if (arr1.get(1).equals(arr2.get(0))) {
+                        lines.add(new LineItem(Type.REMOVED, arr1.get(0)));
+                        arr1.remove(0);
+                    }
+                }
+
+                if (arr2.size() > 0) {
+                    if (arr1.get(0).equals(arr2.get(1))) {
+                        lines.add(new LineItem(Type.ADDED, arr2.get(0)));
+                        arr2.remove(0);
+                    }
+                }
+            }
+        }
+
+        if (arr1.size()>0){
+            lines.add(new LineItem(Type.REMOVED, arr1.get(0)));
+        }
+
+        if (arr2.size()>0){
+            lines.add(new LineItem(Type.ADDED, arr2.get(0)));
+        }
+
+    }
 
     public static enum Type {
         ADDED,        //добавлена новая строка
--- 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -4,9 +4,62 @@ package com.javarush.task.task19.task1917;
 Свой FileWriter
 */
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileConsoleWriter {
+    private FileWriter fileWriter;
     public static void main(String[] args) {
 
+    }
+    FileConsoleWriter(File file) throws IOException {
+        fileWriter = new FileWriter(file);
+    }
+
+    FileConsoleWriter(File file, boolean append) throws IOException {
+        fileWriter = new FileWriter(file, append);
+}
+
+    FileConsoleWriter(FileDescriptor fd) throws IOException {
+        fileWriter = new FileWriter(fd);
+    }
+
+    FileConsoleWriter(String fileName) throws IOException {
+        fileWriter = new FileWriter(fileName);
+    }
+
+    FileConsoleWriter(String fileName, boolean append) throws IOException {
+        fileWriter = new FileWriter(fileName, append);
+    }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        this.fileWriter.write(cbuf, off, len);
+        for (int i = off; i < len+off; i++) {
+            System.out.print(cbuf[i]);
+        }
+    }
+    public void write(int c) throws IOException {
+        this.fileWriter.write(c);
+        System.out.println(c);
+    }
+    public void write(String str) throws IOException {
+        this.fileWriter.write(str);
+        System.out.println(str);
+    }
+    public void write(String str, int off, int len) throws IOException {
+        this.fileWriter.write(str, off, len);
+        System.out.print(str.substring(off,len+off));
+    }
+    public void write(char[] cbuf) throws IOException {
+            this.fileWriter.write(cbuf);
+        for (int i = 0; i < cbuf.length; i++ ) {
+            System.out.print(cbuf[i]);
+        }
+    }
+    public void close() throws IOException {
+        this.fileWriter.close();
     }
 
 }
