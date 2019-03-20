1c1a9cac2bd3c380ec82fa7f856fb77d34712648
Aleksey Tarakanov <zcrewhtc@gmail.com>
2019-03-20 07:13:20 +0300

    Решаю из дома

--- 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -1,18 +1,19 @@
-package com.javarush.task.task19.task1919;
+package com.javarush.task.task19.task1920;
 
 /* 
-Считаем зарплаты
+Самый богатый
 */
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         FileReader input = new FileReader(args[0]);
-        TreeMap<String, Double> map = new TreeMap<>();
+        HashMap<String, Double> map = new HashMap<>();
         String text = "";
         while (input.ready())
             text += (char) input.read();
@@ -24,11 +25,18 @@ public class Solution {
             else {
                 for (Map.Entry<String, Double> each : map.entrySet())
                     if (each.getKey().equals(arr[i].split(" ")[0]))
-                        each.setValue(each.getValue()+Double.parseDouble(arr[i].split(" ")[1]));
+                        each.setValue(each.getValue() + Double.parseDouble(arr[i].split(" ")[1]));
             }
         }
+        double maxvalue = 0;
         for (Map.Entry<String, Double> each : map.entrySet())
-            System.out.println(each.getKey()+" "+each.getValue());
+            if (each.getValue() > maxvalue)
+                maxvalue = each.getValue();
+        TreeMap<String, Double> map1 = new TreeMap<>(map);
+        for (Map.Entry<String, Double> each : map.entrySet())
+            if (each.getValue() != (maxvalue))
+                map1.remove(each.getKey());
+        for (Map.Entry<String, Double> each : map1.entrySet())
+            System.out.println(each.getKey());
     }
-
 }
