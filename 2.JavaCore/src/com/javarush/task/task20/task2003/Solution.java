1fc92e36a10e3ce87a214d416e84b34513e898d5
Aleksey Tarakanov <zcrewhtc@gmail.com>
2019-03-25 06:39:21 +0300

    Решаю из дома

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task20.task2003;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /* 
 Знакомство с properties
@@ -10,18 +11,30 @@ import java.util.Map;
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
+
+    public void fillInPropertiesMap() throws Exception {
         //implement this method - реализуйте этот метод
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-
+        FileInputStream input = new FileInputStream(r.readLine());
+        r.close();
+        load(input);
     }
 
     public void save(OutputStream outputStream) throws Exception {
         //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        prop.putAll(properties);
+        prop.store(outputStream, null);
+        outputStream.close();
     }
 
     public void load(InputStream inputStream) throws Exception {
         //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        prop.load(inputStream);
+        for (String name : prop.stringPropertyNames()) {
+            properties.put(name, prop.getProperty(name)); }
+        inputStream.close();
     }
 
     public static void main(String[] args) {
