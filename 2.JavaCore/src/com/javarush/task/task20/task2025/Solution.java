65ae3a12fcd08901b7095c03f964b8301817cdb6
Aleksey Tarakanov <zcrewhtc@gmail.com>
2019-04-15 10:36:21 +0300

    Решаю из дома

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -1,15 +1,32 @@
 package com.javarush.task.task20.task2025;
 
-/* 
+/*
 Алгоритмы-числа
 */
 public class Solution {
     public static long[] getNumbers(long N) {
+        long counter = 0;
+        for (long i = 1; i<N; i++) {
+            long len = String.valueOf(i).length();
+            char sum;
+            long total = 0;
+            for (int j = 0; j < len; j++) {
+                sum = String.valueOf(i).charAt(j);
+                total += Math.pow(Integer.parseInt(String.valueOf(sum)), len);
+                if (j == len-1) {
+                    if (total == i) {
+                        counter++;
+                        System.out.print(total+" ");
+                    }
+                    total = 0;
+                }
+            }
+        }
         long[] result = null;
         return result;
     }
 
     public static void main(String[] args) {
-
+        long[] x = getNumbers(2147483647);
     }
 }
